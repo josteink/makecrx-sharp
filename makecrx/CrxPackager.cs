11b53028c43991e66b86bb649cad82b32120c09b
Jostein Kjønigsen <jostein@kjonigsen.net>
2016-10-27 14:04:50 +0200

    CrxPackager:
    - basic structure. Everything except crypto implemented.

--- makecrx/CrxPackager.cs
+++ makecrx/CrxPackager.cs
@@ -1,12 +1,63 @@
 ﻿using System;
+using System.IO;
 
 namespace makecrx
 {
+    /// <summary>
+    /// Implementation of CRX package-format according to
+    /// https://developer.chrome.com/extensions/crx
+    /// </summary>
     public class CrxPackager
     {
-        public void Package(string sourceDir, RsaUtil rsaUtil, string targetPath)
+        public void Package(string zipFileName, RsaUtil rsaUtil, string targetPath)
         {
-            throw new NotImplementedException();
+            // get parts
+            var magic = GetMagic();
+            var version = GetVersion();
+            // TODO: GetPublicKey();
+            var publicKey = new byte[0];
+            // TODO: GetSignature();
+            var signature = new byte[0];
+            var zipFile = File.ReadAllBytes(zipFileName);
+
+            // write file
+            using (var fs = File.OpenWrite(targetPath))
+            {
+                WriteBytes(fs, magic);
+                WriteBytes(fs, version);
+                WriteInt32(fs, publicKey.Length);
+                WriteInt32(fs, signature.Length);
+                WriteBytes(fs, publicKey);
+                WriteBytes(fs, signature);
+                WriteBytes(fs, zipFile);
+            }
+        }
+
+        private byte[] GetMagic()
+        {
+            return new byte[] { 0x43, 0x72, 0x32, 0x34 }; // Cr24
+        }
+
+        private byte[] GetVersion()
+        {
+            return new byte[] { 0x02, 0x00, 0x00, 0x00 }; // Cr24
+        }
+
+        private void WriteBytes(Stream stream, byte[] bytes)
+        {
+            stream.Write(bytes, 0, bytes.Length);
+        }
+
+        private void WriteInt32(Stream stream, int value)
+        {
+            byte b1, b2, b3, b4;
+            b1 = (byte)(value & 255);
+            b2 = (byte)((value >> 8) & 255);
+            b3 = (byte)((value >> 16) & 255);
+            b4 = (byte)((value >> 24) & 255);
+
+            var bytes = new byte[] { b1, b2, b3, b4 };
+            WriteBytes(stream, bytes);
         }
     }
 }
